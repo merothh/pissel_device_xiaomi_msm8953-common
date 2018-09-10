9bde8eb16e224077046c58d372cc893ed524c40b
merothh <merothh@gmail.com>
2018-09-10 09:27:07 +0000

    msm8953-common: ship GoogleCameraMod / Enable HAL3
       * https://www.celsoazevedo.com/files/android/google-camera/f/MGC_5.3.015_A7.0+.apk

--- prop.mk
+++ prop.mk
@@ -49,6 +49,7 @@ camera.display.umax=1920x1080 \
 camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
 camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
+persist.camera.HAL3.enabled=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
